9052456450ff9b89e09fcf404f669fd05a727da5
Dan Kuester <daniel.kuester@nist.gov>
2024-07-05 11:49:45 -0600

    the sweep script runs with the updated channel_analysis configuration

--- scripts/sweep.py
+++ scripts/sweep.py
@@ -1,28 +1,31 @@
+from edge_sensor import structs
+from edge_sensor.radio import airt
+import labbench as lb
+from channel_analysis import waveform
 import xarray as xr
 
-from edge_sensor.radio import airt
-from edge_sensor import structs
-from channel_analysis import waveform
-import labbench as lb
-
 lb.show_messages('info')
-sdr = airt.AirT7201B()
 
-run_spec, variables = structs.read_yaml_sweep('run.yaml')
-
+run_spec, sweep_fields = structs.read_yaml_sweep('notebooks/run.yaml')
 data = []
 
-with sdr:
+with airt.AirT7201B() as sdr:
     for capture in run_spec.captures:
-        coords = {f: [getattr(capture, f)] for f in variables}
-        print(coords)
-        count = sdr.configure(capture)
-        iq = sdr.acquire(count)
-        analysis = (
-            waveform
-            .analyze_by_spec(iq, sdr, spec=run_spec.channel_analysis)
-            .assign_coords(coords)
-        )
+        coords = {f: [getattr(capture, f)] for f in sweep_fields}
+        desc = ', '.join([f'{k}={v[0]}' for k,v in coords.items()])
+
+        with lb.stopwatch(f'{desc}: '):
+            sdr.arm(capture)
+            iq = sdr.acquire()
+            analysis = (
+                waveform
+                .analyze_by_spec(iq, capture, spec=run_spec.channel_analysis)
+                .assign_coords(coords)
+            )
+
+        for f in sweep_fields:
+            del analysis.attrs[f]
+
         data.append(analysis)
 
 data = xr.combine_by_coords(data)
