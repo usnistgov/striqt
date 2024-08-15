6f12d5f5a24f653ee6016fbae5f280c55739520b
Kuester <dkuester@nist.gov>
2024-08-15 08:00:20 -0600

    tweaks

--- src/edge_sensor/iq_corrections.py
+++ src/edge_sensor/iq_corrections.py
@@ -179,9 +179,10 @@ def resampling_correction(
             drop=True,
         )
 
-        print(sel)
-
-        power_scale = float(sel.interp(center_frequency=capture.center_frequency))
+        if capture.center_frequency in sel.center_frequency:
+            power_scale = float(sel.sel(center_frequency=capture.center_frequency))
+        else:
+            power_scale = float(sel.interp(center_frequency=capture.center_frequency))
 
     if fft_size == fft_size_out:
         # nothing to do here
