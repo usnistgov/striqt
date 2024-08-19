76f0dc93d24ffe7b070b27b2a18b8b0cab51633e
Kuester <dkuester@nist.gov>
2024-08-19 15:13:52 -0600

    remove unecessary extra __init__ import thanks to iqwaveform update

--- src/edge_sensor/__init__.py
+++ src/edge_sensor/__init__.py
@@ -1,11 +1,6 @@
 """this module deals with the integration of sensor operation on sensor hardware"""
 
 # work around a dynamic library loading packaging quirk on jetson aarch64
-
-import iqwaveform
-
-del iqwaveform
-
 from . import radio, structs
 
 from .io import load, dump, read_yaml_sweep
