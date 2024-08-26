9bb2af1a2817217adf525fed5a6e0cb634f24456
Dan Kuester <daniel.kuester@nist.gov>
2024-08-26 09:59:18 -0600

    copy in schematics code for documentation

---
+++ src/sensor_schematics/frontend.py
@@ -0,0 +1,65 @@
+import schemdraw
+from schemdraw import dsp
+from .rx_blocks import RxBlock, SDR, Mixer
+from iqwaveform import powtodB, dBtopow
+from .schematics import copy_element
+
+def cascade(*blocks: RxBlock) -> RxBlock:
+    """return an RxBlock representing the design estimate for the cascaded performance"""
+    if len(blocks) == 1:
+        return blocks[0]
+    elif len(blocks) > 1:
+        # recursively cascade 2 blocks at a time
+        b0, b1 = blocks[:2]
+
+        # see Pozar, "Microwave Engineering", 3rd. ed., p. 508, eq (10.54)
+        iip3_dBm = -powtodB(dBtopow(-b0.iip3_dBm) + dBtopow(b0.G_dB - b1.iip3_dBm))
+
+        # Friis noise equation
+        NF_dB = powtodB(dBtopow(b0.NF_dB) + (dBtopow(b1.NF_dB)-1)/dBtopow(b0.G_dB))
+
+        if None in (b0.G_dB, b1.G_dB):
+            G_dB = None
+        else:
+            G_dB = b0.G_dB + b1.G_dB
+
+        b0b1 = RxBlock(G_dB=G_dB, NF_dB=NF_dB, iip3_dBm=iip3_dBm)
+        return cascade(b0b1, *blocks[2:])
+    elif len(blocks) == 0:
+        raise TypeError('must pass at least one block')
+
+
+def draw(*blocks: RxBlock, sdr_TX_as_LO: bool = True):
+    """draw the frontend consisting of the cascaded chain of system blocks"""
+    # ref: https://schemdraw.readthedocs.io/en/0.6.0/gallery/gallery.html#superheterodyne-receiver
+    with schemdraw.Drawing() as d:
+        d.config(fontsize=10)
+        L = d.unit/3
+
+        antenna = dsp.Antenna()
+
+        symbols = {}
+
+        for block in blocks:
+            dsp.Line(arrow='->').right(L)
+
+            # confusion results if we don't make fresh copies of these objects
+            symbol = copy_element(block.symbol)
+            symbols.setdefault(type(block), []).append(symbol)
+
+            if block.name is not None:
+                symbol = symbol.label(block.name, loc='top')
+            if isinstance(block, SDR):
+                symbol.anchor('RX1')
+
+            d.add(symbol)
+
+        if sdr_TX_as_LO and SDR in symbols and Mixer in symbols:
+            mixer_symbol = symbols[Mixer][0]
+            sdr_symbol = symbols[SDR][0]
+
+            dsp.Line(xy=sdr_symbol.TXRX2).tox(mixer_symbol.S)
+            dsp.Line(arrow='->').toy(mixer_symbol.S)
+
+    return d
+
---
+++ src/sensor_schematics/rx_blocks.py
@@ -0,0 +1,174 @@
+from dataclasses import dataclass
+from schemdraw import dsp
+from . import schematics
+
+@dataclass
+class RxBlock:
+    NF_dB: float = None # noise figure
+    G_dB: float = None # available power gain
+    oip3_dBm: float = float('inf') # output-referred IP3
+    iip3_dBm: float = float('inf') # input-referred IP3
+    name: str = None
+    symbol = None
+
+    def __post_init__(self):
+        # calculate the missing ip3 value from the input or output
+        if self.oip3_dBm == float('inf') and self.iip3_dBm == float('inf'):
+            return
+        elif self.iip3_dBm != float('inf') and self.G_dB is not None:
+            self.oip3_dBm = self.iip3_dBm + self.G_dB
+        elif self.iip3_dBm != float('inf') and self.G_dB is not None:
+            self.iip3_dBm = self.oip3_dBm - self.G_dB
+
+@dataclass
+class Amplifier(RxBlock):
+    symbol = dsp.Amp(fill='lightblue')
+
+@dataclass
+class Mixer(RxBlock):
+    symbol = dsp.Mixer(fill='navajowhite')
+
+@dataclass
+class PassiveBlock(RxBlock):
+    NF_dB = None
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.NF_dB = -self.G_dB
+
+@dataclass
+class Filter(PassiveBlock):
+    symbol = dsp.Filter(response='bp', lblofst=.2, fill='thistle')
+
+@dataclass
+class Attenuator(PassiveBlock):
+    symbol = schematics.Attenuator(l=0)
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        if self.name is None:
+            self.name = f'{-self.G_dB} dB'
+
+@dataclass
+class SDR(RxBlock):
+    symbol = dsp.Ic(
+        pins=[
+            dsp.IcPin(name='TXRX2', side='left'),
+            dsp.IcPin(name='RX2', side='left'),
+            dsp.IcPin(name='TXRX1', side='left'),
+            dsp.IcPin(name='RX1', side='left')
+        ],
+        leadlen=0,
+        size=(2,3),
+    )
+
+#%% Amplifiers
+minicircuits_zx60_83ln_s = Amplifier(
+    # https://www.minicircuits.com/pdfs/ZX60-83MP-S+.pdf
+    name='Minicircuits\nZX60-83MP-S+',
+    G_dB= 21.9,
+    oip3_dBm=35,
+    NF_dB=1.6
+)
+
+minicircuits_zx60_83mp_s = Amplifier(
+    # https://www.minicircuits.com/pdfs/ZX60-83LN-S+.pdf
+    name='Minicircuits\nZX60-83LN-S+',
+    G_dB=20.,
+    oip3_dBm=40.,
+    NF_dB=3.2
+)
+
+minicircuits_pga_103p = Amplifier(
+    # https://www.minicircuits.com/pdfs/PGA-103+.pdf
+    # estimated @ 3.7 GHz
+    name='Minicircuits\nPGA-103+',
+    G_dB=7.,
+    oip3_dBm=47.5,
+    NF_dB=1.2
+)
+
+analog_hmc994apm5e = Amplifier(
+    # https://www.analog.com/media/en/technical-documentation/data-sheets/hmc994apm5e.pdf
+    name='Analog\nHMC409LP4',
+    G_dB=14,
+    oip3_dBm=40.,
+    NF_dB=5
+)
+
+analog_hmc409lp4 = Amplifier(
+    # https://www.analog.com/media/en/technical-documentation/data-sheets/hmc409.pdf
+    name='Analog\nHMC409LP4',
+    G_dB=32,
+    oip3_dBm=46.,
+    NF_dB=5.8
+)
+
+microchip_mma053aa = Amplifier(
+    # https://ww1.microchip.com/downloads/aemDocuments/documents/RFDS/ProductDocuments/DataSheets/MMA053AA.pdf
+    name="Microchip\nMMA053AA",
+    G_dB=17.,
+    NF_dB=4.,
+    oip3_dBm=43.
+)
+
+microchip_maap011327 = Amplifier(
+    # this is just the chip - hoping to find an eval board
+    # https://cdn.macom.com/datasheets/MAAP-011327.pdf
+    name="Microchip\nMAAP011327",
+    G_dB=13.,
+    NF_dB=4.,
+    oip3_dBm=45.
+)
+
+marki_adm_8350psm = Amplifier(
+    # https://markimicrowave.com/products/surface-mount/amplifiers/adm-8350psm/datasheet/
+    # @ 3.5 GHz
+    name='Marki\nADM 8350PSM',
+    G_dB=23.,
+    NF_dB=2.,
+    oip3_dBm=38.
+)
+
+#%% Filters
+anatech_ab2619b1221 = Filter(
+    name='Anatech\nAB2619B1221\n2614-2624\nMHz',
+    G_dB=-3.0
+)
+
+minicircuits_bpf_v1000 = Filter(
+    # a potential LO filter (ceramic SMT package)
+    name='930-1080\nMHz',
+    G_dB=-3.0
+)
+
+minicircuits_vlf_800 = Filter(
+    # https://www.minicircuits.com/pdfs/VLF-800+.pdf
+    # LO filter that we have on hand for prototyping
+    name='0-1080\nMHz',
+    G_dB=-1.5,
+)
+
+reactel_8C7_3610_x180s11 = Filter(
+    # as seen in the SEA project
+    name='Reactel\n8C7_3610_x180s11\n3550-3700\nMHz',
+    G_dB=-1.0
+)
+
+#%% Mixers
+# For these:
+#    G_dB == -(conversion loss in dB)
+#    NF_dB == (conversion loss in dB)
+marki_t3a_07pa = Mixer(
+    name='Marki\nT3A-07PA',
+    G_dB=-6.5,
+    NF_dB=6.5,
+    iip3_dBm=32.
+)
+
+#%% SDRs
+ettus_e313 = SDR(
+    name='SDR\nUSRP E313',
+    NF_dB=8.
+)
