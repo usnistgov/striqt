f47156f3c128951190e063c369e0becc9188d73b
Dan Kuester <daniel.kuester@nist.gov>
2024-09-27 09:39:09 -0600

    correct an exception with histogram edge generation

--- src/channel_analysis/measurements/_channel_power_histogram.py
+++ src/channel_analysis/measurements/_channel_power_histogram.py
@@ -30,8 +30,10 @@ def make_power_histogram_bin_edges(power_low, power_high, power_resolution, xp=n
         + power_resolution / 2
     )
 
+
+    top_edge = xp.array([power_high + power_resolution / 2])
     return xp.concatenate(
-        [bin_centers - power_resolution, [power_high + power_resolution / 2]]
+        (bin_centers - power_resolution, top_edge)
     )
 
 
