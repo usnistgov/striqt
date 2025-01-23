b0789f0151699b5ed8a1789c823769e4e1991f7a
Dan Kuester <daniel.kuester@nist.gov>
2025-01-22 17:02:13 -0700

    tune deepwave enable delay

--- src/edge_sensor/radios/deepwave.py
+++ src/edge_sensor/radios/deepwave.py
@@ -25,7 +25,7 @@ class Air7x01B(SoapyRadioDevice):
     rx_channel_count = attr.value.int(2, inherit=True)
 
     # set based on gain setting sweep tests
-    _transient_holdoff_time = attr.value.float(2e-3, inherit=True)
+    _transient_holdoff_time = attr.value.float(30e-3, inherit=True)
 
     # stream setup and teardown for channel configuration are slow;
     # instead, stream all RX channels
@@ -33,7 +33,7 @@ class Air7x01B(SoapyRadioDevice):
 
     # without this, multichannel acquisition start time will vary
     # across channels, resulting in streaming errors
-    _rx_enable_delay = attr.value.float(0.28, inherit=True)
+    _rx_enable_delay = attr.value.float(0.3, inherit=True)
     _reenable_cycles = 0
 
     def open(self):
