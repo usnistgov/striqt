567e16a03af2cbaab313d9a236ebe7a646a92ab0
Dan Kuester <daniel.kuester@nist.gov>
2025-01-28 15:47:48 -0700

    more locking

--- src/edge_sensor/radios/deepwave.py
+++ src/edge_sensor/radios/deepwave.py
@@ -56,8 +56,11 @@ class Air7x01B(soapy.SoapyRadioDevice):
         if enable:
             # improved the IQ imbalance (and its repeatability)
             # by about 15 dB in lab tests
+            import numpy as np
+            buf = np.empty((2,2), dtype='float32')
             for _ in range(self._reenable_cycles):
                 self.backend.activateStream(self._rx_stream)
+                self._read_stream([buf[0], buf[1]], 0, 1, 10e-3)
                 self.backend.deactivateStream(self._rx_stream)
                 lb.sleep(0.1)
 
