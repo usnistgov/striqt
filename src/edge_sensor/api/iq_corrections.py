123b3c0367d822d27bdf665af81342cf9188c84d
Dan Kuester <daniel.kuester@nist.gov>
2025-01-30 14:46:09 -0700

    clear debug

--- src/edge_sensor/api/iq_corrections.py
+++ src/edge_sensor/api/iq_corrections.py
@@ -184,7 +184,6 @@ def _describe_missing_data(corrections: 'xr.Dataset', exact_matches: dict):
 def lookup_power_correction(
     cal_data: Path | 'xr.Dataset' | None, capture: structs.RadioCapture, xp
 ):
-    print('!', repr(cal_data), capture, xp)
     if isinstance(cal_data, xr.Dataset):
         corrections = cal_data
     elif cal_data:
