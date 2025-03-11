fix a long-standing slight resampling bug with amplitude scaling

src/edge_sensor/api/iq_corrections.py
@@ -294,6 +294,7 @@ def resampling_correction(
 
     fs, _, analysis_filter = design_capture_filter(radio.base_clock_rate, capture)
     nfft = analysis_filter['nfft']
+    size_in = iq.size
 
     nfft_out, noverlap, overlap_scale, _ = iqwaveform.fourier._ola_filter_parameters(
         iq.size,
@@ -305,8 +306,18 @@ def resampling_correction(
 
     except_on_low_memory()
 
+    needs_stft = base.needs_stft(analysis_filter, capture)
+
+    if not np.isfinite(capture.analysis_bandwidth):
+        filter_domain = None
+    elif base.FILTER_DOMAIN == 'auto':
+        filter_domain = 'frequency' if needs_stft else 'time'
+    else:
+        filter_domain = base.FILTER_DOMAIN
+
     # apply the filter here, where the size of y is minimized
-    if np.isfinite(capture.analysis_bandwidth):
+    if filter_domain == 'time':
+        lb.util.logger.debug('applying filter in time domain')
         h = iqwaveform.design_fir_lpf(
             bandwidth=capture.analysis_bandwidth,
             sample_rate=fs,
@@ -317,8 +328,8 @@ def resampling_correction(
         iq = iqwaveform.oaconvolve(iq, h[xp.newaxis, :], 'full', axes=1)
         iq = iq[:, h.size // 2 :]
 
-    if not base.needs_stft(analysis_filter, capture):
-        # no filtering or resampling needed
+    if not needs_stft:
+        # bail here if filtering or resampling needed
         size = round(capture.duration * capture.sample_rate)
         iq = iq[:, :size]
         if power_scale is not None:
@@ -337,44 +348,48 @@ def resampling_correction(
         return_axis_arrays=False,
     )
 
+    # resample
     except_on_low_memory()
-
-    # first, any operations that reduce the size of y
     if nfft_out < nfft:
+        # downsample by trimming frequency
         freqs = iqwaveform.fftfreq(nfft, 1 / fs)
         freqs, y = iqwaveform.fourier.downsample_stft(
             freqs, y, nfft_out=nfft_out, axis=axis, out=y
         )
-
-    # now upsample if needed
     elif nfft_out > nfft:
-        # upsample
+        # upsample by zero-padding frequency
         pad_left = (nfft_out - nfft) // 2
         pad_right = pad_left + (nfft_out - nfft) % 2
-
         y = iqwaveform.util.pad_along_axis(y, [[pad_left, pad_right]], axis=axis + 1)
 
+    if filter_domain == 'frequency':
+        lb.util.logger.debug('applying filter in frequency domain')
+        y = iqwaveform.fourier.stft_fir_lowpass(
+            y,
+            sample_rate=capture.sample_rate,
+            bandwidth=capture.analysis_bandwidth,
+            transition_bandwidth=500e3,
+            axis=axis,
+            out=y
+        )
     del iq
 
+    # reconstruct into a resampled waveform
     except_on_low_memory()
-
     iq = iqwaveform.istft(
         y, nfft=nfft_out, noverlap=noverlap, axis=axis, overwrite_x=True
     )
+    scale = iq.size/size_in
 
-    except_on_low_memory()
-
-    # start the capture after the transient holdoff window
-    iq_size_out = round(capture.duration * capture.sample_rate)
+    # start the capture after the padding for transients
+    size_out = round(capture.duration * capture.sample_rate)
     i0 = noverlap
-    assert i0 + iq_size_out <= iq.shape[axis]
-    iq = iqwaveform.util.axis_slice(iq, i0, i0 + iq_size_out, axis=axis)
+    assert i0 + size_out <= iq.shape[axis]
+    iq = iqwaveform.util.axis_slice(iq, i0, i0 + size_out, axis=axis)
 
-    if power_scale is None and nfft == nfft_out:
-        pass
-    elif power_scale is None:
-        iq *= np.sqrt(nfft_out / nfft)
-    else:
-        iq *= np.sqrt(power_scale) * np.sqrt(nfft_out / nfft)
+    # apply final scaling
+    if power_scale is not None:
+        scale *= np.sqrt(power_scale)
+    iq *= scale
 
     return iq

src/edge_sensor/api/radio/testing.py
@@ -72,14 +72,14 @@ class SingleToneSource(TestSource):
     )
 
     def get_waveform(self, count, start_index: int, *, channel: int = 0, xp=np):
-        i = xp.arange(start_index, count + start_index, dtype='uint64')
+        i = xp.arange(start_index, count + start_index, dtype='int64')
         f_cw = self.baseband_frequency
         fs = self.backend_sample_rate()
         lo = lo_shift_tone(i, self, xp)
 
         phi = (2 * np.pi * f_cw) / fs * i + np.pi / 2
         ret = lo * xp.exp(1j * phi)
-        ret = ret.astype('complex64')
+        ret = ret.astype(self._transport_dtype)
 
         if self.snr is not None:
             capture = channel_analysis.Capture(duration=self.duration, sample_rate=fs)
@@ -305,8 +305,8 @@ class FileSource(TestSource):
     def open(self):
         self._file_stream = None
 
-    def setup(self, radio_setup: structs.RadioSetup):
-        super().setup(radio_setup)
+    def setup(self, radio_setup: structs.RadioSetup = None, **kws) -> structs.RadioSetup:
+        radio_setup = super().setup(radio_setup, **kws)
 
         if self._file_stream is not None:
             self._file_stream.close()
@@ -323,6 +323,8 @@ class FileSource(TestSource):
         self._iq_capture = msgspec.convert(
             self._file_stream.get_metadata(), structs.FileSourceCapture
         )
+
+        return radio_setup
 
     def arm(self, capture=None, **capture_kws):
         if self._file_stream is None: