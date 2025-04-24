f20e940602124f7308bd9452919f9fc44f7fe616
Dan Kuester <daniel.kuester@nist.gov>
2025-04-24 10:17:04 -0600

    debug moment

--- src/edge_sensor/api/radio/base.py
+++ src/edge_sensor/api/radio/base.py
@@ -487,6 +487,7 @@ class RadioDevice(lb.Device):
         prep_calls = {'buffers': lb.Call(alloc_empty_iq, self, capture)}
         iqwaveform.power_analysis.Any  # touch to work around a lazy loading bug
         if capture != getattr(self, '_armed_capture', None):
+            print('rearming!')
             prep_calls['arm'] = lb.Call(self.arm, capture)
         buffers = lb.concurrently(**prep_calls)['buffers']
 
