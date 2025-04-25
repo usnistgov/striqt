de50d5ed780cee474c2c415bdb5819085d958a2f
Dan Kuester <daniel.kuester@nist.gov>
2025-04-25 13:21:04 -0600

    first stab at a sweeping rework

--- src/edge_sensor/api/sweeps.py
+++ src/edge_sensor/api/sweeps.py
@@ -91,7 +91,7 @@ def design_warmup_sweep(
     )
 
 
-def iq_is_reusable(
+def _iq_is_reusable(
     c1: structs.RadioCapture | None, c2: structs.RadioCapture, base_clock_rate
 ):
     """return True if c2 is compatible with the raw and uncalibrated IQ acquired for c1"""
@@ -123,30 +123,210 @@ def iq_is_reusable(
     return c1_compare == c2_compare
 
 
-def _acquire_recycled(radio, iq, capture_this, capture_next=None):
-    capture_ret = msgspec.structs.replace(
-        capture_this,
-        backend_sample_rate=radio.backend_sample_rate(),
-        start_time=None,
-    )
+class SweepIterator:
+    def __init__(self,
+        radio: RadioDevice,
+        sweep: structs.Sweep,
+        *,
+        calibration: 'xr.Dataset' = None,
+        always_yield=False,
+        quiet=False,
+        pickled=False,
+        loop=False,
+        reuse_compatible_iq=False,
+    ):
+        self.radio = radio
 
-    if capture_next is not None:
-        radio.arm(capture_next)
+        self._calibration=calibration,
+        self._always_yield=always_yield,
+        self._quiet=quiet,
+        self._pickled=pickled,
+        self._loop=loop,
+        self._reuse_iq=reuse_compatible_iq
 
-    return iq, capture_ret
+        self._ext_arm = None
+        self._ext_acquire = None
+        self._ext_intake = None
+
+        self.setup(sweep)
+
+    def set_callbacks(self, *, arm_func, acquire_func, intake_func):
+        self._ext_arm = arm_func
+        self._ext_acquire = acquire_func
+        self._ext_intake = intake_func
+
+    def setup(self, sweep: structs.Sweep):
+        self.sweep: structs.Sweep = sweep
+
+        attrs = {
+            # metadata fields
+            **structs.struct_to_builtins(sweep.radio_setup),
+            **structs.struct_to_builtins(sweep.description),
+        }
+
+        self._analyze = xarray_ops.ChannelAnalysisWrapper(
+            radio=self.radio,
+            sweep=sweep,
+            analysis_spec=sweep.channel_analysis,
+            extra_attrs=attrs,
+            correction=True,
+        )
+
+    def __iter__(self) -> typing.Generator['xr.Dataset' | bytes | None]:
+        iq = None
+        this_ext_data = {}
+        prior_ext_data = {}
+        sweep_time = None
+        capture_prev = None
+        analysis = None
+
+        if self._loop:
+            capture_iter = itertools.cycle(self.sweep.captures)
+            count = float('inf')
+        else:
+            capture_iter = self.sweep.captures
+            count = len(self.sweep.captures)
+
+        if count == 0:
+            return
+
+        # iterate across (previous, current, next) captures to support concurrency
+        offset_captures = util.zip_offsets(capture_iter, (-2, -1, 0, 1), fill=None)
+
+        for i, (_, _, capture_this, capture_next) in enumerate(offset_captures):
+            calls = {}
+
+            if capture_this is None:
+                # Nones at the end indicate post-analysis and saves
+                pass
+            else:
+                calls['acquire'] = lb.Call(
+                    self._acquire,
+                    iq,
+                    capture_prev,
+                    capture_this,
+                    capture_next
+                )
+
+            if capture_prev is None:
+                # no pending data in the first iteration
+                pass
+            else:
+                calls['analyze'] = lb.Call(
+                    self._analyze,
+                    iq,
+                    sweep_time=sweep_time,
+                    capture=capture_prev,
+                    pickled=self._pickled,
+                    overwrite_x=True
+                )
+                calls['prior_ext_data'] = lb.Call(lambda: this_ext_data).rename('prior_ext_data')
+
+            if analysis is None:
+                # for the first two iterations, there is no data to save
+                pass
+            else:
+                calls['intake'] = lb.Call(self._intake, data=analysis, ext_data=prior_ext_data)
+
+            desc = channel_analysis.describe_capture(
+                capture_this, capture_prev, index=i, count=count
+            )
+
+            with lb.stopwatch(f'{desc} •', logger_level='debug' if self._quiet else 'info'):
+                ret = lb.concurrently(**calls, flatten=False)
+
+            if 'analyze' in ret:
+                analysis = ret['analyze']
+
+            if 'acquire' in ret:
+                iq, capture_prev = ret['acquire']['radio']
+                this_ext_data = ret['acquire'].get('extension', {}) or {}
+
+                if not capture_prev.host_resample:
+                    assert capture_prev.sample_rate == capture_prev.backend_sample_rate
+
+            if 'intake' in ret:
+                yield ret['intake']
+            elif self._always_yield:
+                yield None
+
+            if sweep_time is None:
+                sweep_time = capture_prev.start_time
+
+    def _acquire(self, iq_prev, capture_prev, capture_this, capture_next):
+        if self._reuse_iq:
+            reuse_this = _iq_is_reusable(capture_prev, capture_this, self.radio.base_clock_rate)
+            reuse_next = _iq_is_reusable(capture_this, capture_next, self.radio.base_clock_rate)
+        else:
+            reuse_this = reuse_next = False
+
+        if capture_prev is None:
+            self._arm(capture_this)
+
+        # acquire from the radio and any peripherals
+        acquire_calls = {}
+        if reuse_this:
+            capture_ret = msgspec.structs.replace(
+                capture_this,
+                backend_sample_rate=self.radio.backend_sample_rate(),
+                start_time=None,
+            )
+            acquire_calls['radio'] = lb.Call(tuple, iq_prev, capture_ret)
+        else:
+            acquire_calls['radio'] = lb.Call(
+                self.radio.acquire,
+                capture_this,
+                correction=False,
+            )
+
+        if self._ext_acquire is not None:
+            acquire_calls['extension'] = lb.Call(self._ext_acquire, capture_next, self.sweep.radio_setup)
+
+        result = lb.concurrently(**acquire_calls, flatten=False)
+
+        if capture_next is not None and not reuse_next:
+            self._arm(capture_next)
+
+        return result
+
+    def _arm(self, capture):
+        arm_calls = {}
+
+        arm_calls['radio'] = lb.Call(self.radio.arm, capture)
+        if self._ext_arm is not None:
+            arm_calls['extension'] = lb.Call(self._ext_arm, capture, self.sweep.radio_setup)
+
+        return lb.concurrently(**arm_calls)
+
+    def _intake(self, radio_data: 'xr.Dataset', ext_data={}):
+        if not isinstance(radio_data, xr.Dataset):
+            raise ValueError('expected xr.Dataset type for data')
+
+        if len(ext_data) > 0:
+            update_ext_dims = {xarray_ops.CAPTURE_DIM: radio_data.capture.size}
+            new_arrays = {
+                k: xr.DataArray(v).expand_dims(update_ext_dims) for k, v in ext_data.items()
+            }
+            radio_data = radio_data.assign(new_arrays)
+
+        if self._ext_intake is not None:
+            self._ext_intake(radio_data)
+
+        return radio_data
 
 
 def iter_sweep(
     radio: RadioDevice,
     sweep: structs.Sweep,
+    *,
     calibration: 'xr.Dataset' = None,
     always_yield=False,
     quiet=False,
     pickled=False,
     loop=False,
-    reuse_compatible_iq=False,
-) -> typing.Generator['xr.Dataset' | bytes | None]:
-    """iterate through sweep captures on the specified radio, yielding a dataset for each.
+    reuse_compatible_iq=False,        
+) -> SweepIterator:
+    """iterate through sweep captures on the radio, yielding a dataset for each.
 
     Normally, for performance reasons, the first iteration consists of
     `(capture 1) ➔ concurrent(capture 2, analysis 1) ➔ (yield analysis 1)`.
@@ -166,103 +346,36 @@ def iter_sweep(
         An iterator of analyzed data
     """
 
-    attrs = {
-        # metadata fields
-        **structs.struct_to_builtins(sweep.radio_setup),
-        **structs.struct_to_builtins(sweep.description),
-    }
+    return SweepIterator(**locals())
 
-    analyze = xarray_ops.ChannelAnalysisWrapper(
-        radio=radio,
-        sweep=sweep,
-        analysis_spec=sweep.channel_analysis,
-        extra_attrs=attrs,
-        correction=True,
-    )
 
-    if len(sweep.captures) == 0:
-        return
+def iter_callbacks(
+    sweep_iter: SweepIterator,
+    sweep_spec: structs.Sweep = None,
+    *,
+    arm_func: callable[[structs.Capture, structs.RadioSetup], None] | None = None,
+    acquire_func: callable[[structs.Capture, structs.RadioSetup], None] | None = None,
+    intake_func: callable[['xr.Dataset', structs.Capture], typing.Any] | None = None,
+):
+    """trigger callbacks on each sweep iteration.
+    This can add support for external device setup and acquisition. Each callback should be able
+    to accommodate `None` values as sentinels to indicate that no data is available yet (for `save`)
+    or no data being acquired (for `setup` and `acquire`).
 
-    iq = None
-    sweep_time = None
-    capture_prev = None
-    reuse_this = False
-    reuse_next = False
+    Args:
+        sweep_iter: a generator returned by `iter_sweep`
+        sweep_spec: the sweep specification for `sweep_iter`
+        setup: function to be called during before the start of each capture
+        acquire: function to be called during the acquisition of each capture
+        save: function to be called after the acquisition and analysis of each capture
 
-    base_clock_rate = radio.base_clock_rate
+    Returns:
+        Generator
+    """
 
-    if loop:
-        capture_iter = itertools.cycle(sweep.captures)
-        count = float('inf')
-    else:
-        capture_iter = sweep.captures
-        count = len(sweep.captures)
+    sweep_iter.set_callbacks(arm_func=arm_func, acquire_func=acquire_func, intake_func=intake_func)
 
-    # iterate across (previous, current, next) captures to support concurrency
-    offset_captures = util.zip_offsets(capture_iter, (-1, 0, 1), fill=None)
-
-    if reuse_compatible_iq:
-        radio.arm(sweep.captures[0])
-
-    for i, (_, capture_this, capture_next) in enumerate(offset_captures):
-        calls = {}
-
-        if reuse_compatible_iq:
-            reuse_this = iq_is_reusable(capture_prev, capture_this, base_clock_rate)
-            reuse_next = iq_is_reusable(capture_this, capture_next, base_clock_rate)
-
-        if capture_this is None:
-            # skip one at the end to leave a loop iteration for the last analysis
-            pass
-        elif reuse_this:
-            calls['acquire'] = lb.Call(_acquire_recycled,
-                radio,
-                iq.copy(),
-                capture_this,
-                capture_next=None if reuse_next else capture_next
-            )
-        else:
-            calls['acquire'] = lb.Call(
-                radio.acquire,
-                capture_this,
-                next_capture=None if reuse_next else capture_next,
-                correction=False,
-            )
-        lb.util.logger.warning(f'radio arm and acquire for {capture_this}')
-
-        if capture_prev is None:
-            # no data yet in the first iteration
-            pass
-        else:
-            calls['analyze'] = lb.Call(
-                analyze,
-                iq,
-                sweep_time=sweep_time,
-                capture=capture_prev,
-                pickled=pickled,
-                overwrite_x=True
-            )
-
-        desc = channel_analysis.describe_capture(
-            capture_this, capture_prev, index=i, count=count
-        )
-
-        with lb.stopwatch(f'{desc} •', logger_level='debug' if quiet else 'info'):
-            ret = lb.concurrently(**calls, flatten=False)
-
-        if 'analyze' in ret:
-            yield ret['analyze']
-        elif always_yield:
-            yield None
-
-        if 'acquire' in ret:
-            iq, capture_prev = ret['acquire']
-
-            if not capture_prev.host_resample:
-                assert capture_prev.sample_rate == capture_prev.backend_sample_rate
-
-        if sweep_time is None:
-            sweep_time = capture_prev.start_time
+    return sweep_iter
 
 
 def iter_raw_iq(
@@ -315,94 +428,93 @@ def iter_raw_iq(
         yield iq, capture
 
 
-def stopiter_as_return(iter):
-    try:
-        return next(iter)
-    except StopIteration:
-        return StopIteration
+# def stopiter_as_return(iter):
+#     try:
+#         return next(iter)
+#     except StopIteration:
+#         return StopIteration
 
+# def iter_callbacks(
+#     sweep_iter: 'xr.Dataset' | bytes | None,
+#     sweep_spec: structs.Sweep,
+#     *,
+#     arm_func: callable[[structs.Capture, structs.RadioSetup], None] | None = None,
+#     acquire_func: callable[[structs.Capture, structs.RadioSetup], None] | None = None,
+#     intake_func: callable[['xr.Dataset', structs.Capture], typing.Any] | None = None,
+# ):
+#     """trigger callbacks on each sweep iteration.
 
-def iter_callbacks(
-    sweep_iter: 'xr.Dataset' | bytes | None,
-    sweep_spec: structs.Sweep,
-    *,
-    arm_func: callable[[structs.Capture, structs.RadioSetup], None] | None = None,
-    acquire_func: callable[[structs.Capture, structs.RadioSetup], None] | None = None,
-    intake_func: callable[['xr.Dataset', structs.Capture], typing.Any] | None = None,
-):
-    """trigger callbacks on each sweep iteration.
+#     This can add support for external device setup and acquisition. Each callback should be able
+#     to accommodate `None` values as sentinels to indicate that no data is available yet (for `save`)
+#     or no data being acquired (for `setup` and `acquire`).
 
-    This can add support for external device setup and acquisition. Each callback should be able
-    to accommodate `None` values as sentinels to indicate that no data is available yet (for `save`)
-    or no data being acquired (for `setup` and `acquire`).
+#     Args:
+#         sweep_iter: a generator returned by `iter_sweep`
+#         sweep_spec: the sweep specification for `sweep_iter`
+#         setup: function to be called during before the start of each capture
+#         acquire: function to be called during the acquisition of each capture
+#         save: function to be called after the acquisition and analysis of each capture
 
-    Args:
-        sweep_iter: a generator returned by `iter_sweep`
-        sweep_spec: the sweep specification for `sweep_iter`
-        setup: function to be called during before the start of each capture
-        acquire: function to be called during the acquisition of each capture
-        save: function to be called after the acquisition and analysis of each capture
+#     Returns:
+#         Generator
+#     """
 
-    Returns:
-        Generator
-    """
+#     if arm_func is None:
 
-    if arm_func is None:
+#         def arm_func(capture):
+#             pass
+#     elif not hasattr(arm_func, '__name__'):
+#         arm_func.__name__ = 'arm'
 
-        def arm_func(capture):
-            pass
-    elif not hasattr(arm_func, '__name__'):
-        arm_func.__name__ = 'arm'
+#     if acquire_func is None:
 
-    if acquire_func is None:
+#         def acquire_func(capture):
+#             pass
+#     elif not hasattr(acquire_func, '__name__'):
+#         acquire_func.__name__ = 'acquire'
 
-        def acquire_func(capture):
-            pass
-    elif not hasattr(acquire_func, '__name__'):
-        acquire_func.__name__ = 'acquire'
+#     if intake_func is None:
 
-    if intake_func is None:
+#         def intake_func(data):
+#             return data
 
-        def intake_func(data):
-            return data
+#     elif not hasattr(intake_func, '__name__'):
+#         intake_func.__name__ = 'save'
 
-    elif not hasattr(intake_func, '__name__'):
-        intake_func.__name__ = 'save'
+#     # pairs of (data, capture) from the controller
+#     data_spec_pairs = itertools.zip_longest(
+#         sweep_iter, sweep_spec.captures, fillvalue=None
+#     )
 
-    # pairs of (data, capture) from the controller
-    data_spec_pairs = itertools.zip_longest(
-        sweep_iter, sweep_spec.captures, fillvalue=None
-    )
+#     data = None
+#     this_capture = sweep_spec.captures[0]
+#     last_data = None
 
-    data = None
-    this_capture = sweep_spec.captures[0]
-    last_data = None
+#     while True:
+#         lb.util.logger.warning(f'peripherals arm and acquire for {this_capture}')
+#         if this_capture is not None:
+#             arm_func(this_capture, sweep_spec.radio_setup)
 
-    while True:
-        lb.util.logger.warning(f'peripherals arm and acquire for {this_capture}')
-        if this_capture is not None:
-            arm_func(this_capture, sweep_spec.radio_setup)
+#         returns = lb.concurrently(
+#             lb.Call(stopiter_as_return, data_spec_pairs).rename('data'),
+#             lb.Call(acquire_func, this_capture, sweep_spec.radio_setup).rename(
+#                 'acquire'
+#             ),
+#             lb.Call(intake_func, last_data).rename('save'),
+#             flatten=False,
+#         )
 
-        returns = lb.concurrently(
-            lb.Call(stopiter_as_return, data_spec_pairs).rename('data'),
-            lb.Call(acquire_func, this_capture, sweep_spec.radio_setup).rename(
-                'acquire'
-            ),
-            lb.Call(intake_func, last_data).rename('save'),
-            flatten=False,
-        )
+#         yield returns.get('save', None)
 
-        yield returns.get('save', None)
+#         if returns['data'] is StopIteration:
+#             break
+#         else:
+#             (data, this_capture) = returns['data']
+#             ext_data = returns.get('acquire', {})
 
-        if returns['data'] is StopIteration:
-            break
-        else:
-            (data, this_capture) = returns['data']
-            ext_data = returns.get('acquire', {})
-
-        if isinstance(data, xr.Dataset):
-            new_dims = {xarray_ops.CAPTURE_DIM: data.capture.size}
-            ext_dataarrays = {
-                k: xr.DataArray(v).expand_dims(new_dims) for k, v in ext_data.items()
-            }
-            last_data = data.assign(ext_dataarrays)
+#         if isinstance(data, xr.Dataset):
+#             new_dims = {xarray_ops.CAPTURE_DIM: data.capture.size}
+#             ext_dataarrays = {
+#                 k: xr.DataArray(v).expand_dims(new_dims) for k, v in ext_data.items()
+#             }
+#             last_data = data.assign(ext_dataarrays)
