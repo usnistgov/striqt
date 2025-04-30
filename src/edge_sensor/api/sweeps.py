5afb2f7854be8475943b5896ef0b633ff8dcc933
Dan Kuester <daniel.kuester@nist.gov>
2025-04-30 14:19:10 -0600

    fix import

--- src/edge_sensor/api/sweeps.py
+++ src/edge_sensor/api/sweeps.py
@@ -169,7 +169,7 @@ class SweepIterator:
         self._ext_intake = intake_func
 
     def setup(self, sweep: structs.Sweep):
-        self.sweep: structs.Sweep = sweep
+        self.sweep: structs.Sweep = structs.validated(sweep)
 
         self._analyze = xarray_ops.ChannelAnalysisWrapper(
             radio=self.radio,
