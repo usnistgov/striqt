d7f4d090227dcd0bbe3d303549827aa7c62bb34a
Dan Kuester <daniel.kuester@nist.gov>
2025-05-07 08:35:16 -0600

    scoping cleanup

--- src/channel_analysis/api/registry.py
+++ src/channel_analysis/api/registry.py
@@ -49,6 +49,20 @@ def _results_as_arrays(
     return array
 
 
+@functools.lru_cache()
+def _param_to_field(name, p: inspect.Parameter):
+    """convert an inspect.Parameter to a msgspec.Struct field"""
+    if p.annotation is inspect._empty:
+        raise TypeError(
+            f'to register this function, keyword-only argument "{name}" needs a type annotation'
+        )
+
+    if p.default is inspect._empty:
+        return (name, p.annotation)
+    else:
+        return (name, p.annotation, p.default)
+
+
 class KeywordArguments(structs.StructBase):
     """base class for the keyword argument parameters of an analysis function"""
 
@@ -59,19 +73,6 @@ class ChannelAnalysisRegistry(collections.UserDict):
     def __init__(self, base_struct=None):
         super().__init__()
         self.base_struct = base_struct
-
-    @staticmethod
-    def _param_to_field(name, p: inspect.Parameter):
-        """convert an inspect.Parameter to a msgspec.Struct field"""
-        if p.annotation is inspect._empty:
-            raise TypeError(
-                f'to register this function, keyword-only argument "{name}" needs a type annotation'
-            )
-
-        if p.default is inspect._empty:
-            return (name, p.annotation)
-        else:
-            return (name, p.annotation, p.default)
 
     def __call__(self, xarray_datacls: 'dataarray.DataClass', metadata={}) -> TFunc:
         """add decorated `func` and its keyword arguments in the self.tostruct() schema"""
@@ -135,7 +136,7 @@ class ChannelAnalysisRegistry(collections.UserDict):
                     return result_obj.to_xarray()
 
             sig_kws = [
-                self._param_to_field(k, p)
+                _param_to_field(k, p)
                 for k, p in params.items()
                 if p.kind is inspect.Parameter.KEYWORD_ONLY and not k.startswith('_')
             ]
