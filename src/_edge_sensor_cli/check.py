55ee2bb5f99ec46cd4ac2f432e9bdff648010168
Dan Kuester <daniel.kuester@nist.gov>
2025-05-08 16:21:00 -0600

    ruff format

--- src/_edge_sensor_cli/check.py
+++ src/_edge_sensor_cli/check.py
@@ -2,7 +2,8 @@
 
 import click
 
-@click.command("runtime information about running a sweep")
+
+@click.command('runtime information about running a sweep')
 @click.argument('yaml_path', type=click.Path(exists=True, dir_okay=False))
 def run(yaml_path):
     print('Initializing...')
@@ -12,6 +13,7 @@ def run(yaml_path):
     from pprint import pprint
     from edge_sensor.api.io import _get_default_format_fields
     import labbench as lb
+
     lb.show_messages('warning')
 
     sweep = edge_sensor.read_yaml_sweep(yaml_path)
@@ -22,19 +24,26 @@ def run(yaml_path):
     print('Success!')
 
     print('\n\nExpanded paths')
-    print(60*'=')
+    print(60 * '=')
     expanded_paths = {
         'output.path': sweep.output.path,
         'extensions.import_path': sweep.extensions.import_path,
-        'radio_setup.calibration': sweep.radio_setup.calibration
+        'radio_setup.calibration': sweep.radio_setup.calibration,
     }
     pprint(expanded_paths, width=60)
 
     print('\n\nAlias {field} names and expanded values:')
-    print(60*'=')
-    all_fields = _get_default_format_fields(sweep, radio_id=radio_id, yaml_path=yaml_path)
-    fields = {k: v for k,v in all_fields.items() if k == 'start_time' or k not in sweep.defaults.__struct_fields__}
+    print(60 * '=')
+    all_fields = _get_default_format_fields(
+        sweep, radio_id=radio_id, yaml_path=yaml_path
+    )
+    fields = {
+        k: v
+        for k, v in all_fields.items()
+        if k == 'start_time' or k not in sweep.defaults.__struct_fields__
+    }
     pprint(fields, width=40)
+
 
 if __name__ == '__main__':
     run()
