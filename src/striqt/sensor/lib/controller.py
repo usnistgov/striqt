0a47ee1ceae635278fc00f8c1b9ab3a431ca2f5e
Dan Kuester <daniel.kuester@nist.gov>
2025-05-13 07:41:36 -0600

    correct a missing keyword argument that led to 2x warmup sweeps

--- src/striqt/sensor/lib/controller.py
+++ src/striqt/sensor/lib/controller.py
@@ -146,6 +146,8 @@ class SweepController:
                 sweep_spec, skip=tuple(self.warmed_captures)
             )
 
+            print('warmup sweep: ', warmup_sweep)
+
             if len(warmup_sweep.captures) > 0:
                 prep_msg = self._describe_preparation(sweep_spec)
                 if prep_msg:
