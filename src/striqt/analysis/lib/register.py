c61d2541c95466459060c6e513452e8bd459f30f
Dan Kuester <daniel.kuester@nist.gov>
2025-05-29 09:19:03 -0600

    alignment tweak

--- src/striqt/analysis/lib/register.py
+++ src/striqt/analysis/lib/register.py
@@ -404,6 +404,7 @@ class AlignmentCaller:
     def __call__(
         self, iq: 'iqwaveform.type_stubs.ArrayType', capture: specs.Capture
     ) -> float:
+        print(capture, self.meas_kws)
         return self.info.func(iq, capture, **self.meas_kws)
 
     def max_lag(self, capture):
