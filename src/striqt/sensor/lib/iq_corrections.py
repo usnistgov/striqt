219e36a70c1ef919e7437c4fdf0437368ca468f9
Dan Kuester <daniel.kuester@nist.gov>
2025-05-30 13:50:04 -0600

    debug

--- src/striqt/sensor/lib/iq_corrections.py
+++ src/striqt/sensor/lib/iq_corrections.py
@@ -173,7 +173,7 @@ def resampling_correction(
     size_out = round(capture.duration * capture.sample_rate)
 
     if radio._aligner is not None:
-        # free_cupy_mempool()
+        free_cupy_mempool()
 
         align_start = radio._aligner(iq[:, :size_out], capture)
         offset = round(align_start * capture.sample_rate)
