6521712e5bf4cf2d5d830ef41b747c04a378d48c
Dan Kuester <daniel.kuester@nist.gov>
2025-06-02 16:30:34 -0600

    more debug

--- src/striqt/analysis/lib/dataarrays.py
+++ src/striqt/analysis/lib/dataarrays.py
@@ -373,6 +373,8 @@ def evaluate_by_spec(
 ):
     """evaluate each analysis for the given IQ waveform"""
 
+    print('evaluate ', capture.center_frequency, capture.switch_input)
+
     if isinstance(spec, specs.Analysis):
         spec = spec.validate()
     else:
