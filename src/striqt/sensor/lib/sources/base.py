930b9950b296e2f2906486a00d5dceafeb573879
Dan Kuester <daniel.kuester@nist.gov>
2025-06-02 15:12:28 -0600

    fix channel power time series label:

--- src/striqt/sensor/lib/sources/base.py
+++ src/striqt/sensor/lib/sources/base.py
@@ -121,6 +121,8 @@ def _cast_iq(
 
         buffer = xp.array(buffer)
 
+    print('buffer out: ', buffer[-1, buffer.shape[1]//2:][:10])
+
     # what follows is some acrobatics to minimize new memory allocation and copy
     if dtype_in.kind == 'i':
         # the same memory buffer, interpreted as int16 without casting
@@ -137,7 +139,7 @@ def _cast_iq(
         buffer_out = buffer
 
 
-    print('buffer out: ', buffer_out[-1, buffer_out.shape[1]//2:][:10])
+    
 
     return buffer_out
 
