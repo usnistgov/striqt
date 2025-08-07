5d80a68cff8270f4d90393c0a3f95c37e86a30e0
Dan Kuester <daniel.kuester@nist.gov>
2025-08-07 11:36:56 -0600

    make cupy_max_fft_chunk_size configurable

--- src/striqt/sensor/lib/controller.py
+++ src/striqt/sensor/lib/controller.py
@@ -9,21 +9,15 @@ import rpyc
 from . import captures, sweeps, util
 from . import specs
 from .sources import find_radio_cls_by_name, is_same_resource, SourceBase
-from striqt.analysis.lib.dataarrays import AcquiredIQ
 
 if typing.TYPE_CHECKING:
     import iqwaveform
-    import numpy as np
     import xarray as xr
     import labbench as lb
-    import pandas as pd
-    from striqt import analysis
 else:
-    iqwaveform = util.lazy_import('numpy')
-    np = util.lazy_import('numpy')
+    iqwaveform = util.lazy_import('iqwaveform')
     xr = util.lazy_import('xarray')
     lb = util.lazy_import('labbench')
-    analysis = util.lazy_import('striqt.analysis')
 
 
 def _consume_warmup(controller, radio_setup, gen: typing.Generator[typing.Any]):
