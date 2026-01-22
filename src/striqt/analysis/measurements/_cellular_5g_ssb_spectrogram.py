c19cc5d3772c5f777291a639f08ade89e3a2b78e
Dan Kuester <daniel.kuester@nist.gov>
2026-01-21 17:16:48 -0700

    fix the SSB spectrogram coord

--- src/striqt/analysis/measurements/_cellular_5g_ssb_spectrogram.py
+++ src/striqt/analysis/measurements/_cellular_5g_ssb_spectrogram.py
@@ -50,8 +50,8 @@ def cellular_ssb_baseband_frequency(
             'center_frequency did not match any keys given for frequency_offset'
         )
 
-    nfft = round(capture.sample_rate / spec.subcarrier_spacing)
-    freqs = shared.truncate_spectrogram_bandwidth(
+    freqs = waveform.util.binned_mean(freqs, count=2, axis=0, fft=True)
+    return shared.truncate_spectrogram_bandwidth(
         freqs,
         nfft,
         capture.sample_rate,
