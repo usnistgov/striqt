2355be42f3a6f7499586f78b77016a4128a74868
Dan Kuester <daniel.kuester@nist.gov>
2026-02-05 09:10:17 -0700

    lazy import updates

--- src/striqt/waveform/util.py
+++ src/striqt/waveform/util.py
@@ -91,41 +91,41 @@ def safe_import(name, timeout: float | None = 5, service=True):
     return mod
 
 
-class _LazyLoader(importlib.util.LazyLoader):
-    lock = threading.RLock()
+# class _LazyLoader(importlib.util.LazyLoader):
+#     lock = threading.RLock()
 
-    def __init__(self, loader):
-        super().__init__(loader)
+#     def __init__(self, loader):
+#         super().__init__(loader)
 
-    @classmethod
-    def factory(cls, loader):
-        with cls.lock:
-            return super().factory(loader)
+#     @classmethod
+#     def factory(cls, loader):
+#         with cls.lock:
+#             return super().factory(loader)
 
-    def exec_module(self, module):
-        if threading.current_thread() == threading.main_thread():
-            t0 = time.perf_counter()
-            super().exec_module(module)
-            print('main thread: exec module ', module.__name__, time.perf_counter()-t0)
-        else:
-            print('main thread: skip exec module', module.__name__)
+#     def exec_module(self, module):
+#         if threading.current_thread() == threading.main_thread():
+#             t0 = time.perf_counter()
+#             super().exec_module(module)
+#             print('main thread: exec module ', module.__name__, time.perf_counter()-t0)
+#         else:
+#             print('main thread: skip exec module', module.__name__)
 
-    def create_module(self, spec):
-        if threading.current_thread() == threading.main_thread():
-            t0 = time.perf_counter()
-            print('main thread: create module ', spec.name, time.perf_counter()-t0)
-            return super().create_module(spec)
-        else:
-            print('other thread: skip create module ', spec.name)
+#     def create_module(self, spec):
+#         if threading.current_thread() == threading.main_thread():
+#             print('main thread: create module ', spec.name)
+#             return super().create_module(spec)
+#         else:
+#             print('other thread: skip create module ', spec.name)
 
-    def load_module(self, fullname: str) -> ModuleType:
-        if threading.current_thread() == threading.main_thread():
-            t0 = time.perf_counter()
-            print('main thread: load module ', fullname, time.perf_counter()-t0)
-            return super().load_module(fullname)
-        else:
-            print('other thread: load module ', fullname)
-            return safe_import(fullname)
+#     def load_module(self, fullname: str) -> ModuleType:
+#         if threading.current_thread() == threading.main_thread():
+#             # t0 = time.perf_counter()
+#             ret = super().load_module(fullname)
+#             # print('main thread: load module ', fullname, time.perf_counter()-t0)        
+#             return ret
+#         else:
+#             print('other thread: load module ', fullname)
+#             return safe_import(fullname)
 
 
 def lazy_import(module_name: str, package=None):
@@ -145,8 +145,8 @@ def lazy_import(module_name: str, package=None):
     spec = importlib.util.find_spec(module_name, package=package)
     if spec is None or spec.loader is None:
         raise ImportError(f'no module found named "{module_name}"')
-    # spec.loader = importlib.util.LazyLoader(spec.loader)
-    spec.loader = _LazyLoader(spec.loader)
+    spec.loader = importlib.util.LazyLoader(spec.loader)
+    # spec.loader = _LazyLoader(spec.loader)
     module = importlib.util.module_from_spec(spec)
     sys.modules[module_name] = module
     spec.loader.exec_module(module)
