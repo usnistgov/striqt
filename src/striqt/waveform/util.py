14df8494b262489a49a96e41bc29ca827d52b6f5
Dan Kuester <daniel.kuester@nist.gov>
2026-02-04 17:48:53 -0700

    work toward further reducing import errors

--- src/striqt/waveform/util.py
+++ src/striqt/waveform/util.py
@@ -46,22 +46,22 @@ def lazy_import(module_name: str, package=None):
     until it is used.
     """
     # see https://docs.python.org/3/library/importlib.html#implementing-lazy-imports
-    try:
-        ret = sys.modules[module_name]
-        return ret
-    except KeyError:
-        pass
-
-    spec = importlib.util.find_spec(module_name, package=package)
-    if spec is None or spec.loader is None:
-        raise ImportError(f'no module found named "{module_name}"')
-    # spec.loader = importlib.util.LazyLoader(spec.loader)
-    spec.loader = _LazyLoader(spec.loader)
     with _LazyLoader.lock:
+        try:
+            ret = sys.modules[module_name]
+            return ret
+        except KeyError:
+            pass
+
+        spec = importlib.util.find_spec(module_name, package=package)
+        if spec is None or spec.loader is None:
+            raise ImportError(f'no module found named "{module_name}"')
+        # spec.loader = importlib.util.LazyLoader(spec.loader)
+        spec.loader = _LazyLoader(spec.loader)
         module = importlib.util.module_from_spec(spec)
         sys.modules[module_name] = module
         spec.loader.exec_module(module)
-    return module
+        return module
 
 
 if typing.TYPE_CHECKING:
