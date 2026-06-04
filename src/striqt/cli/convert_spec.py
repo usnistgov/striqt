767592f8552f157c5ea59fa5fcf3a07f3a5a914d
Dan Kuester <daniel.kuester@nist.gov>
2026-06-04 10:38:09 -0600

    suffix fix

--- src/striqt/cli/convert_spec.py
+++ src/striqt/cli/convert_spec.py
@@ -58,7 +58,7 @@ def run(path, yaml: bool):
     if yaml:
         out_path = Path(path).with_suffix('.yaml')
         if str(out_path) == path:
-            out_path = out_path.with_name(path.name + '-flat')
+            out_path = out_path.with_name(out_path.name + '-flat')
         s = msgspec.yaml.encode(spec, enc_hook=sa.specs.helpers._enc_hook)
         with open(out_path, 'wb') as stream:
             stream.write(s)
