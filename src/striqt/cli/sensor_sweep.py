4de3c2d06cc38d4087164ba5e0ef3f39d00ed552
Dan Kuester <daniel.kuester@nist.gov>
2026-06-04 10:15:01 -0600

    clean up sensor_sweep.py to enable usage from test scripts

--- src/striqt/cli/sensor_sweep.py
+++ src/striqt/cli/sensor_sweep.py
@@ -1,91 +1,100 @@
 #!/usr/bin/env python3
 
 """this is installed into the shell PATH environment as configured by pyproject.toml"""
+from prompt_toolkit.shortcuts import confirm
 
 import click
-from . import click_sensor_sweep
-import sys
+from typing import Callable, Union, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import striqt.sensor as ss
 
 
-def adjust_port(spec, port):
-    from striqt import sensor as ss
-
-    loops = []
-    for i, loop in enumerate(spec.loops):
-        if loop.field == 'port':
-            new_loop = ss.specs.List(field='port', values=(port,))
-            loops.append(new_loop)
-            loops.extend(spec.loops[i + 1 :])
-            return spec.replace(loops=loops)
-        else:
-            loops.append(loop)
-
-    # no loop - replace the captures instead
-    captures = [c.replace(port=port) for c in spec.captures]
-    return spec.replace(captures=captures)
-
-
-@click_sensor_sweep('Run a swept acquisition and analysis from a yaml file')
-@click.option(
-    '--port/',
-    default=None,
-    prompt=True,
-    required=False,
-    prompt_required=False,
-    type=click.IntRange(min=0),
-    help='limit the acquisition the specified input port index',
-)
-def run(*, path, debug, skip_confirm, verbose, port, **kws):
+def confirm_labels(spec: 'ss.specs.Sweep', source_id: str):
     import striqt.sensor as ss
     import striqt.analysis as sa
+    from pprint import pformat
 
-    def confirm_labels(sweep: ss.specs.Sweep, source_id: str):
-        from striqt import sensor as ss
-        from pprint import pformat
+    labels = ss.specs.helpers.list_capture_adjustments(spec, source_id)
+    if len(labels) == 0:
+        return
 
-        labels = ss.specs.helpers.list_capture_adjustments(spec, source_id)
+    dict_repr = pformat(labels, indent=2, sort_dicts=False)
+    info = (
+        '▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n'
+        'the following unique field values will be set based on adjust_captures:\n'
+        f' {dict_repr[1:-1]} \n'
+        '▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n'
+    )
 
-        if len(labels) == 0:
-            return
+    while True:
+        answer = sa.util.blocking_input(f'{info}\nconfirm: are these correct? (y/n) ')
+        if answer.lower() == 'y':
+            break
+        elif answer.lower() == 'n':
+            raise KeyboardInterrupt
 
-        dict_repr = pformat(labels, indent=2, sort_dicts=False)
-        info = (
-            '▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n'
-            'the following unique field values will be set based on adjust_captures:\n'
-            f' {dict_repr[1:-1]} \n'
-            '▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n'
-        )
 
-        while not skip_confirm:
-            response = sa.util.blocking_input(
-                f'{info}\nconfirm: are these correct? (y/n) '
-            )
+def run(path: str, *, output_path=None, check_func: Union[Callable, None]=None):
+    import striqt.sensor as ss
 
-            if response.lower() == 'y':
-                break
-            elif response.lower() == 'n':
-                raise KeyboardInterrupt
-
-    except_handler = ss.util.DebugOnException(enable=debug, verbose=verbose)
-    sys.excepthook = except_handler.run
-
-    ss.util.log_verbosity(verbose)
     if path.endswith('yaml'):
-        spec = ss.read_yaml_spec(path, output_path=kws['output_path'])
+        spec = ss.read_yaml_spec(path, output_path=output_path)
     elif path.endswith('json'):
-        spec = ss.read_json_spec(path, output_path=kws['output_path'])
+        spec = ss.read_json_spec(path, output_path=output_path)
     else:
         raise click.ClickException('expected file to have .json or .yaml suffix')
 
-    if port is not None:
-        spec = adjust_port(spec, port)
-
-    ctx = ss.open_resources(spec, path, on_source_opened=confirm_labels)
+    ctx = ss.open_resources(spec, path, on_source_opened=check_func)
     with ctx as resources:
         sweep = ss.iterate_sweep(resources, yield_values=False, always_yield=True)
         for _ in sweep:
             pass
 
 
+@click.command('run a sweep from a json/yaml specification')
+@click.argument('path', type=click.Path(exists=True, dir_okay=False))
+@click.option(
+    '--debug/',
+    '-d',
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help='invoke an IPython debug prompt on exception',
+)
+@click.option(
+    '--skip-confirm/',
+    '-s',
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help='skip y/n confirmation before start',
+)
+@click.option(
+    '--verbose/',
+    '-v',
+    show_default=True,
+    default=False,
+    count=True,
+    help='add detail to messages',
+)
+@click.option(
+    '--output-path/',
+    '-o',
+    default=None,
+    type=click.Path(),
+    help='override the output file path in the yaml specification',
+)
+def cli(path: str, output_path, debug: bool, verbose: bool, skip_confirm: bool):
+    import striqt.sensor as ss
+    import sys
+
+    except_handler = ss.util.DebugOnException(enable=debug, verbose=verbose)
+    sys.excepthook = except_handler.run
+    ss.util.log_verbosity(verbose)
+
+    run(path, output_path=output_path, check_func=None if skip_confirm else confirm_labels)
+
+
 if __name__ == '__main__':
-    run()  # pyright: ignore
+    cli()  # pyright: ignore
