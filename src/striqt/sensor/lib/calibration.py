0a96f16affedcbba98a787ca593bcbd63d7f889c
Dan Kuester <daniel.kuester@nist.gov>
2026-07-17 15:25:32 -0600

    cache all cal data in memory

--- src/striqt/sensor/lib/calibration.py
+++ src/striqt/sensor/lib/calibration.py
@@ -154,9 +154,15 @@ class YFactorSink(sinks.SinkBase):
         assert isinstance(capture_result.extra_coords, specs.SoapyAcquisitionInfo)
 
         if len(self._pending_data) == self._batch.size:
+            # self.flush()
             self._batch.next()
 
         return ret
+
+    def close(self, *exc_info):
+        if exc_info == (None, None, None):
+            self.flush()
+        super().close(*exc_info)
 
     def flush(self):
         data = self.pop()
