5ef6309f2265fd83ad183279b818e1969b6add94
sensor <sensor@radio07>
2026-07-22 11:23:29 -0600

    typing band-aid

--- src/striqt/sensor/specs/structs.py
+++ src/striqt/sensor/specs/structs.py
@@ -495,7 +495,10 @@ class AcquisitionInfo(msgspec.Struct, kw_only=True, frozen=True):
     source_id: types.SourceID = ''
     sweep_index: Union[int, None] = None
     capture_index: int = 0
-    signal_trigger: Union[sa.Trigger, None] = None
+
+    # TODO: this is wrong. want sa.Trigger, but that is triggering a type
+    # resolution problem in py39
+    signal_trigger: Union[str, None] = None
 
     def replace(self, **attrs) -> _Self:
         """returns a copy of self with changed attributes.
